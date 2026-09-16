54124beb6f11d0ac6ce86079bbfd5fd3d6429f62
szouga <szouga9@gmail.com>
2026-09-15 19:40:19 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import sheCodesLogo from "./shecodes_logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={sheCodesLogo}
+          className="App-logo img-fluid"
+          alt="SheCodes Logo"
+        />
+      </header>
+    </div>
+  );
+}
