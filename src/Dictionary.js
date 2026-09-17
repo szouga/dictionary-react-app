166549fd98a009711450116d809a1612c4d18170
szouga <szouga9@gmail.com>
2026-09-16 19:50:07 -0500

    axios, api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for definition of: ${keyword}`);
+    //alert(`Searching for definition of: ${keyword}`);
+    let apiKey = "7e1fbo71a48539tbb0610fa3a35820ef";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
